33abed5f3cbbf089a5fb44565b363ab107ed02ad
Alexander Botkin <axb2@cornell.edu>
2020-02-15 17:40:26 -0800

    Update to the commit that fixes the reversal mistake

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
 //        .package(path: "../Mantle")
-        .package(url: "https://github.com/Mantle/Mantle.git", .revision("f93c6267fc8ea2b314edd8831ac81c84b007ac94"))
+        .package(url: "https://github.com/Mantle/Mantle.git", .revision("10514f54b3b42f76cd87fceca64658ae5cf565aa"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
